7a4826e1e2c388ed884c5ea5a69e1f1a482ef81b
ajayantrix <ajay.antrix@gmail.com>
2022-03-26 08:00:02 +0530

    Added 3 modelling codes model 1, 2 & 3

--- model.py
+++ model.py
@@ -1,3 +1,13 @@
 # Adding all the Model related processing here 
 
 print("here we are in the Model side of things")
+
+
+def model1():
+    print("Adding model 1 here")
+
+def model2():
+    print("Adding model 2 here")
+
+def model3():
+    print("Adding model 3 here")
